a18db45b450f6b0eb54482296bd23321d3305e93
j04vinay <j04vinay@gmail.com>
2022-09-08 00:51:14 +0530

    commit

---
+++ 48rotateImage.cpp
@@ -0,0 +1,26 @@
+/* You are given an n x n 2D matrix representing an image, rotate the image by 90 degrees (clockwise).
+
+You have to rotate the image in-place, which means you have to modify the input 2D matrix directly. DO NOT allocate another 2D matrix and do the rotation.
+
+ 
+
+Example 1:
+Input: matrix = [[1,2,3],[4,5,6],[7,8,9]]
+Output: [[7,4,1],[8,5,2],[9,6,3]]
+*/
+
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int N = matrix.size();
+        for(int i=0;i<N/2;i++){
+            for(int j=i;j<N-1-i;j++){
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[N - 1 - j][i];
+                matrix[N - 1 - j][i] = matrix[N - 1 - i][N - 1 - j];
+                matrix[N - 1 - i][N - 1 - j] = matrix[j][N - 1 - i];
+                matrix[j][N - 1 - i] = temp;
+            }
+        }
+    }
+};
